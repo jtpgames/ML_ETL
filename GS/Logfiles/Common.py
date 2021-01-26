dc8067b0a2b7f503830ae8eceb11d84b668a1713
juritomak <juri.tomak@gmail.com>
2021-01-26 11:06:19 +0100

    Included LogToDbETL.py and its dependencies

--- GS/Logfiles/Common.py
+++ GS/Logfiles/Common.py
@@ -1,6 +1,7 @@
 import argparse
 import logging
 import os
+import re
 from re import search
 from datetime import datetime
 from typing import Dict
@@ -42,4 +43,37 @@ def readResponseTimesFromLogFile(path: str) -> Dict[datetime, float]:
             response_times[time_stamp] = float(response_time) / 1000
 
     return response_times
+
+
+def read_data_line_from_log_file(path: str):
+    with open(path) as logfile:
+        for line in logfile:
+            if 'Response time' not in line:
+                continue
+
+            # Extract:
+            # * Timestamp as DateTime
+            # * Number of Parallel Requests
+            # * Request Type
+            # * Request Execution Time
+
+            time_stamp = datetime.strptime(re.search('\\[.*\\]', line).group(), '[%Y-%m-%d %H:%M:%S,%f]')
+
+            number_of_parallel_requests_s = re.search('(?<=PR:)(\\s*\\d*)/(\\s*\\d*)/(\\s*\\d*)', line).groups()
+            number_of_parallel_requests_start = int(number_of_parallel_requests_s[0])
+            number_of_parallel_requests_end = int(number_of_parallel_requests_s[1])
+            number_of_parallel_requests_finished = int(number_of_parallel_requests_s[2])
+
+            request_type = re.search(r"ID_\w+", line).group()
+
+            response_time = re.search('(?<=Response time\\s)\\d*', line).group()
+
+            yield {
+                "time_stamp": time_stamp,
+                "number_of_parallel_requests_start": number_of_parallel_requests_start,
+                "number_of_parallel_requests_end": number_of_parallel_requests_end,
+                "number_of_parallel_requests_finished": number_of_parallel_requests_finished,
+                "request_type": request_type,
+                "response_time": response_time
+            }
 
---
+++ GS/Logfiles/LogToDbETL.py
@@ -0,0 +1,116 @@
+import sqlite3
+from datetime import datetime
+from glob import glob
+from sqlite3 import Error, Connection, Cursor
+
+from Common import get_date_from_string, read_data_line_from_log_file
+from CommonDb import training_data_exists_in_db, TrainingDataRow, SQLSelectExecutor, create_connection
+
+
+def execute_sql_statement(conn: Connection, statement_sql: str, print_statement: bool = False):
+    """ execute the given sql statement
+        :param conn: Connection object
+        :param statement_sql: an SQL statement
+        :param print_statement: print the sql statement to stdout before executing it
+        :return:
+        """
+    try:
+        if print_statement:
+            print("Executing %s" % statement_sql)
+        c = conn.cursor()
+        c.execute(statement_sql)
+    except Error as e:
+        print("Error while executing %s" % statement_sql)
+        print(e)
+
+
+def construct_create_training_data_table_statement(table_name):
+    statement = f""" 
+    CREATE TABLE IF NOT EXISTS `{table_name}` (
+        `timestamp` TIMESTAMP NOT NULL,
+        `number_of_parallel_requests_start` SMALLINT unsigned NOT NULL,
+        `number_of_parallel_requests_end` SMALLINT unsigned NOT NULL,
+        `number_of_parallel_requests_finished` SMALLINT unsigned NOT NULL,
+        `request_type` VARCHAR NOT NULL,
+        `request_execution_time_ms` INT unsigned NOT NULL,
+        PRIMARY KEY (`timestamp`)
+    );
+    """
+
+    return statement
+
+
+def construct_create_index_statement(table_name, index_name, column):
+    return f"""
+    CREATE INDEX IF NOT EXISTS `{index_name}` 
+    ON {table_name}(`{column}`);
+    """
+
+
+def construct_insert_training_data_statement(table_name, row: TrainingDataRow):
+    statement = f"""
+    INSERT INTO {table_name}(
+        timestamp, 
+        number_of_parallel_requests_start, 
+        number_of_parallel_requests_end,
+        number_of_parallel_requests_finished,
+        request_type,
+        request_execution_time_ms
+    )
+    VALUES(
+        "{row.timestamp}", 
+        {row.number_of_parallel_requests_start},
+        {row.number_of_parallel_requests_end},
+        {row.number_of_parallel_requests_finished},
+        "{row.request_type}",
+        {row.request_execution_time_ms}
+    );
+    """
+
+    return statement
+
+
+def setup_db() -> Connection:
+    db_connection = create_connection(r"db/trainingdata.db")
+
+    if db_connection is None:
+        exit(1)
+
+    execute_sql_statement(
+        db_connection,
+        construct_create_training_data_table_statement("gs_training_data"),
+        True
+    )
+
+    execute_sql_statement(
+        db_connection,
+        construct_create_index_statement(
+            "gs_training_data",
+            "idx_request_type",
+            "request_type"
+        ),
+        True
+    )
+
+    return db_connection
+
+
+if __name__ == '__main__':
+    dbConnection = setup_db()
+
+    for logFile in sorted(glob("data/Conv_2021-*.log")):
+        if not training_data_exists_in_db(dbConnection, logFile):
+            for line in read_data_line_from_log_file(logFile):
+                execute_sql_statement(
+                    dbConnection,
+                    construct_insert_training_data_statement(
+                        "gs_training_data",
+                        TrainingDataRow.from_dict(line)
+                    )
+                )
+            dbConnection.commit()
+            print("Committed")
+        else:
+            print("Skipping ", logFile)
+
+    dbConnection.close()
